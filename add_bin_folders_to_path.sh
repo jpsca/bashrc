aeacd0699d82e3bbfbaf36ed2bc0b5bfd57071b7
Juan-Pablo Scaletti <juanpablo@jpscaletti.com>
2021-07-01 10:57:00 -0500

    Add docker-compose aliases

--- add_bin_folders_to_path.sh
+++ add_bin_folders_to_path.sh
@@ -1,3 +1,2 @@
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
-export PATH="$PATH:/Applications/Sublime Text.app/Contents/SharedSupport/bin/"
--- alias_virtualenv.sh
+++ alias_virtualenv.sh
@@ -1,3 +1,3 @@
 alias on="source .venv/bin/activate"
 alias off="deactivate"
-alias mkenv="python -m venv .venv && on && pip install -U pip"
+alias mkenv="python -m venv .venv && on && pip install -U pip wheel"
